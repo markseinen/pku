7d90709876c7430198a7b6da282babc23a782b73
Mark Prive <markprive@MBP-MarkSeinen.local>
2020-11-21 19:26:54 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
